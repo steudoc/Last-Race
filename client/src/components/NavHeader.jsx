9f34c0da97d95b11334bede267672edd6587bac5
Stefano Tallone <s307569@studenti.polito.it>
2026-06-16 21:59:13 +0200

    Setted navbar disabling when playing

--- client/src/components/NavHeader.jsx
+++ client/src/components/NavHeader.jsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { Button, Container, Navbar, Nav } from "react-bootstrap";
 import { LogoutButton } from "./AuthComponents";
 import { useEffect, useState } from "react";
 
 function NavHeader(props) {
+    const location = useLocation();
+    const isPlaying = location.pathname === '/game/plan';
 
     return (
         <Navbar className="app-navbar" expand="sm">
             <Container>
                 {/* title */}
-                <Navbar.Brand as={Link} to="/">
+                <Navbar.Brand as={Link} to={isPlaying ? "#" : "/"}>
                     <i className="bi bi-train-front text-accent me-2"></i>
                     Last Race
                 </Navbar.Brand>
@@ -19,7 +21,7 @@ function NavHeader(props) {
                     
                     {/* links */}
                     <Nav className="me-auto mt-2 mt-sm-0">
-                        {props.loggedIn && (
+                        {props.loggedIn && !isPlaying && (
                             <>
                                 <Nav.Link as={Link} to="/map">Map</Nav.Link>
                                 <Nav.Link as={Link} to="/game/setup">Play</Nav.Link>
@@ -31,7 +33,7 @@ function NavHeader(props) {
                     {/* login/logout */}
                     <Nav className="align-items-center gap-2 mt-3 mt-sm-0">
                         {props.loggedIn ? (
-                            <LogoutButton handleLogout={props.handleLogout} />
+                            !isPlaying && <LogoutButton handleLogout={props.handleLogout} />
                         ) : (
                             <Link to="/login" className="btn-metro py-1.5 px-3 fs-6 rounded-3">
                                 Sign In
